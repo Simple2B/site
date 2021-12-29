c8c41a29eafcae5b65cb4da9994559f68b6b74ed
Dmitriy Matviichuk <dmitriy.matvv@gmail.com>
2021-12-29 11:26:05 +0200

    [a] navbar component

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -1,6 +1,26 @@
+import clsx from "clsx";
 import React from "react";
+import classes from "./Navbar.module.scss";
+import { menuList } from "../../types/menu";
+import Link from "next/link";
 
 export interface INavbarProps {}
 export const Navbar: React.FC<INavbarProps> = () => {
-  return <>Navbar</>;
+  const menuItems = menuList.map((itm) => {
+    return (
+      <Link key={itm.id} href={itm.url}>
+        <a className={classes.navbar__link}>{itm.title}</a>
+      </Link>
+    );
+  });
+  return (
+    <div className={clsx(classes.navbar)}>
+      <div className="container">
+        <div className={classes.navbar__wrapper}>
+          <span className={classes.navbar__logo}>Logo</span>
+          <nav className={classes.navbar__list}>{menuItems}</nav>
+        </div>
+      </div>
+    </div>
+  );
 };
