343794db4fa9f0a0d9134651160e3a5265e59438
Dmitriy Matviichuk <dmitriy.matvv@gmail.com>
2021-12-29 22:03:18 +0200

    [a] loading svg like react components

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+
+const withReactSvg = require('next-react-svg')
+const path = require('path')
+
+module.exports = withReactSvg({
+  include: path.resolve(__dirname, 'src/assets/svg'),
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config
+  }
+})
