c09805b027cc0fe7a6417da6b4c990463eaf9804
Yurii Palidovych <Palidovych1@gmail.com>
2022-02-09 13:05:56 +0200

    [u]change logo,size font, change button size

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import classes from "./Navbar.module.scss";
 import { menuList } from "../../types/menu";
 import { CustomButton } from "../Buttons/CustomButton";
 import { MenuLink } from "./MenuLink";
-import LogoBlack from "../../assets/svg/logo/logo_blck.svg";
 import { useMediaQuery } from "react-responsive";
 import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
 import Link from "next/link";
 import { useAppContext } from "../../context/state";
+import Image from "next/image";
 
 export interface INavbarProps {}
 export const Navbar: React.FC<INavbarProps> = () => {
@@ -37,11 +37,17 @@ export const Navbar: React.FC<INavbarProps> = () => {
     <nav className={clsx(classes.navbar)}>
       <div className="container">
         <div className={classes.navbar__wrapper}>
-          <span className={classes.navbar__logo}>
+          <span className={classes.navbar__logo_container}>
             {
               <Link href={"/"}>
                 <a>
-                  <LogoBlack />
+                  <Image
+                    src={`/svg/logo/logo_blck.svg`}
+                    alt="Simple2b logo"
+                    layout="fill"
+                    objectFit="cover"
+                    quality={100}
+                  />
                 </a>
               </Link>
             }
@@ -49,7 +55,7 @@ export const Navbar: React.FC<INavbarProps> = () => {
           <div className={classes.navbar__controls}>
             <div className={classes.navbar__list}>{menuItems}</div>
             {/* TODO: add navbar button click handler */}
-            <CustomButton title="Contact Us" onClick={openModal} size="small" />
+            <CustomButton title="Contact Us" onClick={openModal} size="smallForHeader" />
           </div>
         </div>
       </div>
