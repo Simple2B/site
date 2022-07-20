7f6f29af7cd0f3993121efce55d37c878a752e09
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-20 08:09:29 +0300

    model

---
+++ backend/app/model/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa F401
+from .user import User
+
+from app.database import Base
---
+++ backend/app/model/user.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, func, or_
+from sqlalchemy.orm import relationship
+
+from app.hash_utils import make_hash, hash_verify
+from app.database import Base, SessionLocal
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), nullable=False, unique=True)
+    email = Column(String(128), nullable=False, unique=True)
+    password_hash = Column(String(128), nullable=False)
+    created_at = Column(DateTime(), default=datetime.now)
+
+    @property
+    def password(self):
+        return self.password_hash
+
+    @password.setter
+    def password(self, value: str):
+        self.password_hash = make_hash(value)
+
+    @classmethod
+    def authenticate(cls, db: SessionLocal, user_id: str, password: str):
+        user = (
+            db.query(cls)
+            .filter(
+                or_(
+                    func.lower(cls.username) == func.lower(user_id),
+                    func.lower(cls.email) == func.lower(user_id),
+                )
+            )
+            .first()
+        )
+        if user is not None and hash_verify(password, user.password):
+            return user
+
+    def __repr__(self):
+        return f"<{self.id}: {self.username}>"
