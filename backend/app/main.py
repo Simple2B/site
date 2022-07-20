99a40afb1ca7c5b6f45218e36d390612f5fbd521
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-20 08:09:16 +0300

    app

---
+++ backend/app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.router import user, auth
+from .config import settings
+
+app = FastAPI(
+    title=settings.SERVER_NAME,
+    docs_url="/backend/docs",
+    redoc_url="/backend/redoc",
+    openapi_url="/backend/openapi.json",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(user.router)
+app.include_router(auth.router)
+
+
+@app.get("/")
+def root():
+    SAMPLE_ENV_VAR = settings.SAMPLE_ENV_VAR
+    return {"ENV": SAMPLE_ENV_VAR}
