c2c383bfcd16788cc826b60df487841d3d4d74ef
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-21 23:37:40 +0300

    init db

--- backend/tasks/init_db.py
+++ backend/tasks/init_db.py
@@ -39,11 +39,3 @@ def fill_test_data(db: Session):
         db.add(user)
         db.commit()
         db.refresh(user)
-        for pid in range(NUM_TEST_POSTS):
-            db.add(
-                Post(
-                    title=f"Title{pid}",
-                    content=(f"Test of post {pid}" * (pid + 1)),
-                    user=user,
-                )
-            )
