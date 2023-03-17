ff49d36cc054bfcafa93625cf33bad611be47435
nazarKobryn <kobrinnazar05@gmail.com>
2023-03-17 16:22:51 +0200

    create model contact us

--- backend/app/model/contact_us_data.py
+++ backend/app/model/contact_us_data.py
@@ -1,14 +1,17 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime
 from app.database import Base
 
 
-class Message(Base):
-    __tablename__ = "messages"
+class ContactUsData(Base):
+    __tablename__ = "contact_us_data"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     email = Column(String(64), nullable=False)
     message = Column(String(512), nullable=False)
     phone = Column(String(12), nullable=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.now)
 
     def __repr__(self):
-        return f"<Message: {self.name}>"
+        return f"<{self.id}: from {self.name}({self.email})>"
--- backend/app/schema/contact_us.py
+++ backend/app/schema/contact_us.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel, Field, EmailStr
 
 
-class CreateMessage(BaseModel):
+class CreateContactUs(BaseModel):
     name: str = Field(min_length=1, max_length=63)
     email: EmailStr
     message: str = Field(min_length=1, max_length=511)
