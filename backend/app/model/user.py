70f2b4be05e39fe320a8ae8f7ee5be8e80343cd6
nazarKobryn <kobrinnazar05@gmail.com>
2023-03-17 17:13:51 +0200

    fix back

--- backend/app/model/user.py
+++ backend/app/model/user.py
@@ -13,13 +13,13 @@ class User(Base):
     username = Column(String(64), nullable=True)
     email = Column(String(128), nullable=False, unique=True)
     image_url = Column(String(128), nullable=True)
-    git_hub_id = Column(Integer, nullable=False)
+    git_hub_id = Column(String(32), nullable=False)
     created_at = Column(DateTime(timezone=True), default=datetime.now)
     role = Column(Enum(UserRole), default=UserRole.candidate)
 
     @classmethod
     def authenticate(cls, db: SessionLocal, git_hub_id: int):
-        user = db.query(cls).get(git_hub_id)
+        user = db.query(cls).filter_by(git_hub_id=git_hub_id).first()
         if user is not None:
             return user
 
