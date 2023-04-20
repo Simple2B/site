92a809c7f7ac8cdedb554a674bfb660d25d42d11
varvara <varvarashcherbyna7@gmail.com>
2023-04-20 11:40:15 +0300

    build: config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,18 +1,32 @@
-/** @type {import('next').NextConfig} */
+// const { join } = require('path');
+const path = require('path');
 
-const withReactSvg = require('next-react-svg')
-const path = require('path')
+module.exports = () => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    reactStrictMode: false,
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+    output: 'standalone',
+    rewrites: () => {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://app:80/api/:path*', // Proxy to Backend
+        },
+      ];
+    },
 
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, 'src/assets/svg'),
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-    return config
-  },
-  experimental: {
-    outputStandalone: true,
-  },
-})
+    images: {
+      domains: [
+        'find-my-coach-eu.s3.amazonaws.com',
+        'find-my-coach-eu.s3.eu-west-2.amazonaws.com',
+      ],
+      formats: ['image/avif', 'image/webp'],
+    },
+  };
+  return nextConfig;
+};
---
+++ frontend/webpack.config.js
@@ -0,0 +1,3 @@
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+
+export const plugins = [new NodePolyfillPlugin()];
