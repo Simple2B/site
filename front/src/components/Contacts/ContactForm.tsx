7d5ddb59fb4a8f9d4bbb0bb56b307eac33d9f2f9
AlexAzalor <xy55xy.azalor@gmail.com>
2023-06-15 15:14:27 +0300

    refactoring ContactForm: "useState" inputs handler => "react-hook-form"

--- front/src/components/Contacts/ContactForm.tsx
+++ front/src/components/Contacts/ContactForm.tsx
@@ -1,95 +1,61 @@
 "use client";
 
-import clsx from "clsx";
 import { useSession } from "next-auth/react";
-import React, { useEffect, useState } from "react";
-import { useAppContext } from "../../context/state";
+import { useEffect, useState } from "react";
+
+import clsx from "clsx";
+import classes from "./Contacts.module.scss";
+import baseClasses from "../Input/BaseInput.module.scss";
+import baseFileClasses from "../Input/BaseFileInput.module.scss";
+
+import { SubmitHandler, useForm } from "react-hook-form";
 import { quizApi } from "../../services/quizApi";
 import { CustomButton } from "../Buttons/CustomButton";
-import { BaseInput } from "../Input/BaseInput";
-import { BaseFileInput } from "../Input/BaseFileInput";
-import classes from "./Contacts.module.scss";
+import { ControllerFormInput } from "./ControllerFormInput";
+
+const TARGET_HOST = "https://mailer.simple2b.net";
+const DEFAULT_FORM_VALUES = {
+  name: "",
+  email: "",
+  phone: "",
+  message: "",
+  attachment: null,
+}
+const inputWrapperStyle = classes.form__input_wrapper;
+const inputErrorStyle = classes.form__input_error;
 
 export interface IContactFormProps {
   greyBg?: boolean;
 }
-export const ContactForm: React.FC<IContactFormProps> = ({ greyBg }) => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [message, setMessage] = useState("");
-  const [attachment, setAttachment] = useState<File | null>(null);
-  const [errors, setErrors] = useState({
-    name: false,
-    email: false,
-    message: false,
-  });
-  const [success, setSuccess] = useState<boolean | null>(null);
-  const [errorTest, setErrorText] = useState<string>("");
 
+export type Inputs = {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+  attachment: File | null;
+}
+
+export const ContactForm: React.FC<IContactFormProps> = ({ greyBg }) => {
   const { data } = useSession();
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setErrors((prev) => {
-      return { ...prev, name: false };
-    });
-    setName(e.target.value);
-  };
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (email === "")
-      setErrors((prev) => {
-        return { ...prev, email: false };
-      });
-    setEmail(e.target.value);
-  };
-  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (parseInt(e.target.value.slice(-1)) !== 0) {
-      if (!parseInt(e.target.value.slice(-1)) && e.target.value.length > 0)
-        return;
-    }
-    setPhone(e.target.value);
-  };
-  const handleAttachmentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const file: File = e.target.files![0];
-      setAttachment(file);
-    }
-  };
-  const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setErrors((prev) => {
-      return { ...prev, message: false };
-    });
-    setMessage(e.target.value);
-  };
-  const handleSendMessage = async () => {
-    let trigger = false;
-    if (success !== null) return;
-    if (name === "") {
-      setErrors((prev) => {
-        return { ...prev, name: true };
-      });
-      trigger = true;
-    }
-    if (email === "") {
-      setErrors((prev) => {
-        return { ...prev, email: true };
-      });
-      trigger = true;
-    }
-    if (message === "") {
-      setErrors((prev) => {
-        return { ...prev, message: true };
-      });
-      trigger = true;
-    }
-    if (trigger) {
-      console.log("errors :>> ", errors);
-      return;
-    }
+  const [sendEmailError, setSendEmailError] = useState<string>('');
+  const [success, setSuccess] = useState<boolean | null>(null);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+    control,
+  } = useForm<Inputs>({ defaultValues: DEFAULT_FORM_VALUES });
+
+  const onSubmit: SubmitHandler<Inputs> = async (inputsData) => {
+    const { name, email, message, phone, attachment } = inputsData;
+
+    console.log('input data to API: ', inputsData)
     const newMessage = await quizApi.addMessage(name, email, message, phone);
-
-    const TARGET_HOST = "https://mailer.simple2b.net";
 
     const formData = new FormData();
     formData.append("name", name);
@@ -110,107 +76,99 @@ export const ContactForm: React.FC<IContactFormProps> = ({ greyBg }) => {
     if (mailerResponse.status !== 200) {
       const maillerResponseJson = await mailerResponse.json();
       if (maillerResponseJson.message) {
-        setErrorText(maillerResponseJson.message);
+        setSendEmailError(maillerResponseJson.message);
         return;
       }
     }
 
     if (newMessage && mailerResponse.status === 200) {
       setSuccess(true);
-      setName("");
-      setEmail("");
-      setPhone("");
-      setMessage("");
-      setAttachment(null);
+      reset();
+
+      return;
     } else {
-      setSuccess(false);
+      return setSuccess(false);
     }
-  };
+  }
 
   useEffect(() => {
     if (data) {
-      setEmail(data.user?.email!);
-      setName(data.user?.name!);
+      setValue("email", data.user?.email!);
+      setValue("name", data.user?.name!);
     }
-  }, [data]);
+  }, [data, setValue]);
 
-  const buttonText = success === null ? "Send" : success ? "Success" : "Fail";
-  const buttonStyle =
-    success === null ? "normal" : success ? "success" : "fail";
+  const inputStyle = [classes.form_input, greyBg && classes.form_input_grey];
+
+  const isNoSuccess = success === null;
+  const buttonText = isNoSuccess ? "Send" : success ? "Success" : "Fail";
+  const buttonStyle = isNoSuccess ? "normal" : success ? "success" : "fail";
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={classes.form__input_block}>
-        <div className={classes.form__input_wrapper}>
-          <BaseInput
-            value={data ? data.user?.name! : name}
-            placeholder="Name  *"
-            onChange={handleNameChange}
-            style={clsx(classes.form_input, greyBg && classes.form_input_grey)}
-            required
+        <ControllerFormInput
+          name="name"
+          placeholder="Name*"
+          control={control}
+          data={data ? data.user?.name! : null}
+          error={errors.name}
+          backgroundStyle={greyBg}
+        />
+
+        <ControllerFormInput
+          name="email"
+          placeholder="Email*"
+          type="email"
+          control={control}
+          data={data ? data.user?.email! : null}
+          error={errors.email}
+          backgroundStyle={greyBg}
+        />
+
+        <ControllerFormInput
+          name="phone"
+          placeholder="Phone*"
+          type="number"
+          control={control}
+          error={errors.phone}
+          backgroundStyle={greyBg}
+        />
+
+        <div className={inputWrapperStyle}>
+          <input
+            {...register("message", { required: true })}
+            placeholder="Message*"
+            className={clsx(baseClasses.base, ...inputStyle)}
           />
-          {errors.name && (
-            <span className={classes.form__input_error}>
-              This field is required
-            </span>
-          )}
+
+          {errors.message && <span className={inputErrorStyle}>This field is required</span>}
         </div>
-        <div className={classes.form__input_wrapper}>
-          <BaseInput
-            value={data ? data.user?.email! : email}
-            type="email"
-            placeholder="E-mail  *"
-            onChange={handleEmailChange}
-            style={clsx(classes.form_input, greyBg && classes.form_input_grey)}
-          />
-          {errors.email && (
-            <span className={classes.form__input_error}>
-              This field is required
-            </span>
-          )}
-        </div>
-        <div className={classes.form__input_wrapper}>
-          <BaseInput
-            value={phone}
-            type="tel"
-            placeholder="Phone number"
-            onChange={handlePhoneChange}
-            style={clsx(classes.form_input, greyBg && classes.form_input_grey)}
-          />
-        </div>
-        <div className={classes.form__input_wrapper}>
-          <BaseInput
-            value={message}
-            placeholder="Message  *"
-            onChange={handleMessageChange}
-            style={clsx(classes.form_input, greyBg && classes.form_input_grey)}
-          />
-          {errors.message && (
-            <span className={classes.form__input_error}>
-              This field is required
-            </span>
-          )}
-        </div>
-        <div className={classes.form__input_wrapper}>
-          <BaseFileInput
-            files={attachment && [attachment]}
+
+        <div className={inputWrapperStyle}>
+          <input
+            {...register("attachment")}
+            type="file"
+            id="file-upload"
             placeholder="Attachment"
-            onChange={handleAttachmentChange}
-            style={clsx(classes.form_input, greyBg && classes.form_input_grey)}
+            className={clsx(baseFileClasses.base, ...inputStyle)}
           />
         </div>
+
         <div className={classes.contacts__wrapper}>
-          {errorTest && (
-            <span className={classes.form__input_error}>{errorTest}</span>
+          {sendEmailError && (
+            <span className={inputErrorStyle}>{sendEmailError}</span>
           )}
         </div>
+
+        <CustomButton
+          title={buttonText}
+          size="large"
+          onClick={() => { }}
+          type="filled"
+          status={buttonStyle}
+        />
       </div>
-      <CustomButton
-        title={buttonText}
-        size="large"
-        onClick={handleSendMessage}
-        type="filled"
-        status={buttonStyle}
-      />
-    </>
+    </form>
   );
 };
---
+++ front/src/components/Contacts/ControllerFormInput.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import clsx from "clsx";
+import classes from "./Contacts.module.scss";
+import baseClasses from "../Input/BaseInput.module.scss";
+import { Control, Controller, FieldError } from "react-hook-form"
+import { Inputs } from "./ContactForm";
+
+interface Props {
+  control: Control<Inputs, string>;
+  data?: string | null;
+  error?: FieldError;
+  name: "name" | "email" | "phone";
+  placeholder: string;
+  type?: string;
+  backgroundStyle?: boolean;
+}
+
+export const ControllerFormInput = (
+  { name,
+    placeholder,
+    type, control,
+    data,
+    error,
+    backgroundStyle
+  }: Props) => {
+  const inputStyle = [baseClasses.base, classes.form_input, backgroundStyle && classes.form_input_grey];
+
+  return (
+    <div className={classes.form__input_wrapper}>
+      <Controller
+        control={control}
+        name={name}
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <input
+            onChange={onChange}
+            value={data ? data : value}
+            className={clsx(...inputStyle)}
+            placeholder={placeholder}
+            type={type}
+          />
+        )}
+      />
+
+      {error && <span className={classes.form__input_error}>This field is required</span>}
+    </div>
+  )
+}
