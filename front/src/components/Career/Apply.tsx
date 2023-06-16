a97a17377f9b41310a818a7e256106d30a1260d0
nazarKobryn <kobrinnazar05@gmail.com>
2023-06-16 10:49:52 +0300

    update auth

--- front/src/components/Career/Apply.tsx
+++ front/src/components/Career/Apply.tsx
@@ -3,18 +3,18 @@ import { getServerSession } from "next-auth/next";
 import { options } from "@/app/options";
 import NavigateBtn from "../Buttons/NavigateBtn";
 
-const Apply = async () => {
+const BtnApply = async () => {
   const session = await getServerSession(options);
 
   return (
     <div>
       {session ? (
-        <NavigateBtn title="Apply" pushTo={"/careers"} />
+        <NavigateBtn title="Apply" pushTo={"careers"} />
       ) : (
-        <NavigateBtn title="Sign In to apply" pushTo={"/singin"} />
+        <NavigateBtn title="Sign In to apply" pushTo={"singin"} />
       )}
     </div>
   );
 };
 
-export default Apply;
+export default BtnApply;
