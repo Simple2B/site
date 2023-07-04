0e2e7ec90646336843a6b4fdd527740601e0dcee
nazarKobryn <kobrinnazar05@gmail.com>
2023-07-04 15:48:16 +0300

    updated middle were

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -2,18 +2,14 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
-  // Getting cookies from the request using the `RequestCookies` API
-
-  // Setting cookies on the response using the `ResponseCookies` API
-  console.log("request ------------------------->")
-  // console.log(request)
-  console.log(request.nextUrl.hostname , "hostname")
-  console.log(request.referrer, "referrer -domaine ")
-  // console.log(request)
-  console.log("request ------------------------->")
   const response = NextResponse.next()
-  response.cookies.set('n18i', 'en')
+  let language = "en"
+  const domaineNameUrl = request.headers.get("referer")
+
+  if (domaineNameUrl && domaineNameUrl.includes("dev.simple2b.com")) {
+    language = "de"
+  }
+  response.cookies.set('n18i', language)
   return response
 }
 
