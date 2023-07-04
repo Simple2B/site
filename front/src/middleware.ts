72e7a545f49305c50e0b7f8d4eeabe5e10b4dc3b
nazarKobryn <kobrinnazar05@gmail.com>
2023-07-04 16:03:06 +0300

    updated middleware

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -5,6 +5,7 @@ export function middleware(request: NextRequest) {
   const response = NextResponse.next()
   let language = "en"
   const domaineNameUrl = request.headers.get("referer")
+  console.log("domaineNameUrl", domaineNameUrl)
 
   // TODO just exemple of how to get the domain name and set the language
   if (domaineNameUrl && domaineNameUrl.includes("dev.simple2b.net")) {
