d069a92bfb3eacc3c64883c7793b9f9a2939399e
AlexAzalor <xy55xy.azalor@gmail.com>
2023-07-05 15:02:12 +0300

    [fix git actions] config update

--- back_v2/app/config.py
+++ back_v2/app/config.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
     SAMPLE_ENV_VAR: str = "<None>"
     JWT_SECRET: str = "<None>"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DB_URI: str  # use for test
-    DATABASE_URI: str = ""
+    DB_URI: str = ""  # use for test
+    DATABASE_URI: str = "sqlite:///./dev.db"
     COUNT_OF_QUESTION: int = 25
     DEV_DATABASE_URI: str = ""
     ADMIN_USER: str = "admin"
