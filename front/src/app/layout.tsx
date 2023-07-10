dca14e20659288d36262c2fb98fc39b429b242c2
AlexAzalor <xy55xy.azalor@gmail.com>
2023-07-10 10:49:39 +0300

    meta tags and robots

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,11 +1,17 @@
 import "./styles/globals.css";
 import Providers from "./providers";
+import { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL("https://www.simple2b.com"),
   title: {
     template: "%s | Simple2B",
     default: "Simple2B", // a default is required when creating a template
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
   },
   openGraph: {
     title: "Simple2B",
@@ -13,6 +19,7 @@ export const metadata = {
     description:
       "We help businesses to succeed through innovative and reliable solutions.",
     type: "website",
+    images: ['https://www.simple2b.net/png/logo.png'],
   },
   twitter: {
     card: "summary_large_image",
@@ -21,11 +28,16 @@ export const metadata = {
       "We help businesses to succeed through innovative and reliable solutions.",
     images: [`https://www.simple2b.net/png/logo.png`],
   },
-  app: {
-    name: "twitter_app",
-    url: {
-      iphone: "https://www.simple2b.net/",
-      ipad: "https://www.simple2b.net/",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
     },
   },
 };
@@ -37,21 +49,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* // TODO it */}
-      {/* <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-      />
-      <meta
-        property="og:image"
-        content={`https://www.simple2b.net/png/logo.png`}
-      />
-      <meta property="og:email" content="simple2b.info@gmail.com" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;900&display=swap"
-        rel="stylesheet"
-      /> */}
       {/* suppressHydrationWarning={true} */}
       <body>
         <Providers>{children}</Providers>
