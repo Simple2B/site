d42c3c2365659cfe80874fe67e2627accbd1ebef
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-07-14 12:43:38 +0300

    remove: frontend

--- frontend/prisma/migrations/20220208174137_fixed_attempt/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Results" DROP CONSTRAINT "Results_attemptId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Results" ADD CONSTRAINT "Results_attemptId_fkey" FOREIGN KEY ("attemptId") REFERENCES "Attempt"("id") ON DELETE SET NULL ON UPDATE CASCADE;
