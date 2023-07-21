a65809e873d199d3c02fe3fc1fcef8c26298bb17
AlexAzalor <xy55xy.azalor@gmail.com>
2023-07-21 18:33:21 +0300

    add logs

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -9,11 +9,14 @@ async function auth(req: any, res: NextApiResponse) {
     console.log('|||||| 0 |||||| resssssssssssssss ||||||||||||', res);
     console.log('|||||| 1 |||||| req ||||||||||||', req);
     console.log('|||||| 2 |||||| req.headers.get referer ||||||||||||', req.headers.get('referer'));
-    console.log('|||||| 3 |||||| req.headers.get host ||||||||||||', req.headers.get('host'));
+    console.log('|||||| 3 |||||| req headers ||||||||||||', req.headers);
     console.log('|||||| 4 |||||| req.headers.get accept ||||||||||||', req.headers.get('accept'));
     console.log('|||||| 5 |||||| req.headers.get accept-language ||||||||||||', req.headers.get('accept-language'));
     console.log('|||||| 6 |||||| req.headers.get x-forwarded-host ||||||||||||', req.headers.get('x-forwarded-host'));
-    console.log('|||||| 7 |||||| req.client?.baseUrl ||||||||||||', req.client?.baseUrl);
+    console.log('|||||| 7 |||||| req.client?.origin ||||||||||||', req.client?.origin);
+    console.log('|||||| 8 |||||| req state ||||||||||||', req.state);
+    console.log('|||||| 9 |||||| req cookies ||||||||||||', req.cookies);
+
 
     return await NextAuth(req, res, options(req.headers.get('referer')));
   }
