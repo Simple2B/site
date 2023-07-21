7f9d0511932fad98a57be8ecffab40d64bc517dd
AlexAzalor <xy55xy.azalor@gmail.com>
2023-07-21 20:03:40 +0300

    update logs

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -6,16 +6,11 @@ import { NextRequest } from "next/server";
 
 async function auth(req: any, res: NextApiResponse) {
   if (req instanceof Request || NextRequest) {
-    console.log('|||||| 0 |||||| resssssssssssssss ||||||||||||', res);
-    console.log('|||||| 1 |||||| req ||||||||||||', req);
+    // console.log('|||||| 0 |||||| resssssssssssssss ||||||||||||', res);
+    // console.log('|||||| 1 |||||| req ||||||||||||', req);
     console.log('|||||| 2 |||||| req.headers.get referer ||||||||||||', req.headers.get('referer'));
-    console.log('|||||| 3 |||||| req headers ||||||||||||', req.headers);
-    console.log('|||||| 4 |||||| req.headers.get accept ||||||||||||', req.headers.get('accept'));
-    console.log('|||||| 5 |||||| req.headers.get accept-language ||||||||||||', req.headers.get('accept-language'));
-    console.log('|||||| 6 |||||| req.headers.get x-forwarded-host ||||||||||||', req.headers.get('x-forwarded-host'));
-    console.log('|||||| 7 |||||| req.client?.origin ||||||||||||', req.client?.origin);
-    console.log('|||||| 8 |||||| req state ||||||||||||', req.state);
-    console.log('|||||| 9 |||||| req cookies ||||||||||||', req.cookies);
+    // console.log('|||||| 3 |||||| req headers ||||||||||||', req.headers);
+    console.log('|||||| 9 |||||| req cookies ||||||||||||', req.cookies.get('n18i'));
 
 
     return await NextAuth(req, res, options(req.headers.get('referer')));
