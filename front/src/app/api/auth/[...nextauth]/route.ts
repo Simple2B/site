c160aeaf5de89187114a673ac61e0e760f99d99f
AlexAzalor <xy55xy.azalor@gmail.com>
2023-07-21 18:02:44 +0300

    try ti find german hostname 3

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -6,8 +6,14 @@ import { NextRequest } from "next/server";
 
 async function auth(req: any, res: NextApiResponse) {
   if (req instanceof Request || NextRequest) {
-    console.log('|||||| 1 |||||| req.realm ||||||||||||', req);
-    console.log('|||||| 2 |||||| req.headers.get ||||||||||||', req.headers.get('referer'));
+    console.log('|||||| 0 |||||| resssssssssssssss ||||||||||||', res);
+    console.log('|||||| 1 |||||| req ||||||||||||', req);
+    console.log('|||||| 2 |||||| req.headers.get referer ||||||||||||', req.headers.get('referer'));
+    console.log('|||||| 3 |||||| req.headers.get host ||||||||||||', req.headers.get('host'));
+    console.log('|||||| 4 |||||| req.headers.get accept ||||||||||||', req.headers.get('accept'));
+    console.log('|||||| 5 |||||| req.headers.get accept-language ||||||||||||', req.headers.get('accept-language'));
+    console.log('|||||| 6 |||||| req.headers.get x-forwarded-host ||||||||||||', req.headers.get('x-forwarded-host'));
+    console.log('|||||| 7 |||||| req.client?.baseUrl ||||||||||||', req.client?.baseUrl);
 
     return await NextAuth(req, res, options(req.headers.get('referer')));
   }
