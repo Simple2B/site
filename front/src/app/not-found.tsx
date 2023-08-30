5279f34d2df6433b1c29f43c1a3a57a366d36cb0
nazarKobryn <kobrinnazar05@gmail.com>
2023-08-30 18:04:42 +0300

    fix not found not completed

--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -1,31 +1,28 @@
 import Image from 'next/image';
-import { MainLayout } from '@/components';
 import { IMG_DOMAIN_SERVER } from './constants-server';
 import NavigateBtn from '@/components/Buttons/NavigateBtn';
 
 export default function NotFound() {
   return (
-    <MainLayout hideFooter={true}>
-      <div className="flex justify-center items-center flex-col h-[80vh] mx-6">
-        <Image
-          src={`${IMG_DOMAIN_SERVER}/error/404.svg`}
-          alt="404 error page"
-          width={522}
-          height={300}
-        />
+    <div className="flex justify-center items-center flex-col h-[80vh] mx-6">
+      <Image
+        src={`${IMG_DOMAIN_SERVER}/error/404.svg`}
+        alt="404 error page"
+        width={522}
+        height={300}
+      />
 
-        <h1 className="font-semibold text-4xl text-center">
-          Oops! <br />
-          Page not found!
-        </h1>
+      <h1 className="font-semibold text-4xl text-center">
+        Oops! <br />
+        Page not found!
+      </h1>
 
-        <h2 className="text-center w-80 my-6">
-          This page doesn’t exist or was removed. We suggest you to go back to
-          main.
-        </h2>
+      <h2 className="text-center w-80 my-6">
+        This page doesn’t exist or was removed. We suggest you to go back to
+        main.
+      </h2>
 
-        <NavigateBtn title="Go To Main" size="large" pushTo="/" />
-      </div>
-    </MainLayout>
+      <NavigateBtn title="Go To Main" size="large" pushTo="/" />
+    </div>
   );
 }
