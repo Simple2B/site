cbcf8692f5a6bc20c700a6d2e91ce1da2e839835
nazarKobryn <kobrinnazar05@gmail.com>
2023-10-06 10:22:14 +0300

    fixed style case image

--- front/src/components/CaseCard/CaseCard.tsx
+++ front/src/components/CaseCard/CaseCard.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
+import Link from 'next/link';
+import Image from 'next/image';
 
-import classes from "./Case.module.scss";
-import { CaseOut } from "@/openapi";
+import classes from './Case.module.scss';
+import { CaseOut } from '@/openapi';
 
 export interface ICaseCardProps {
   card: CaseOut;
@@ -19,16 +19,16 @@ export const CaseCard = ({ card }: ICaseCardProps) => {
         <h4 className={classes.case_card__title}>{card.title}</h4>
         <p className={classes.case_card__description}>{card.description}</p>
       </div>
-      <span className={classes.case_card__illustration}>
+      <div className={classes.case_card__illustration}>
         <Image
           src={card.previewImageUrl}
           alt="xcv"
           width={0}
           height={0}
           sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
+          style={{ width: '100%', height: '100%' }}
         />
-      </span>
+      </div>
     </Link>
   );
 };
