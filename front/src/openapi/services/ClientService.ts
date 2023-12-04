b3d25291ab2cf1d000f237c74ce6b20608c401e5
nazarKobryn <kobrinnazar05@gmail.com>
2023-12-04 14:48:21 +0200

    added send mail by lang

--- front/src/openapi/services/ClientService.ts
+++ front/src/openapi/services/ClientService.ts
@@ -1,31 +1,34 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-import type { Body_contact_form } from '../models/Body_contact_form';
 import type { CandidateAnswerOut } from '../models/CandidateAnswerOut';
 
 import type { CancelablePromise } from '../core/CancelablePromise';
 import { OpenAPI } from '../core/OpenAPI';
 import { request as __request } from '../core/request';
+import { Languages } from '../models/Languages';
 
 export class ClientService {
   /**
    * Contact Form
    * @param formData
    * @param candidateUuid
+   * @param language
    * @returns CandidateAnswerOut Successful Response
    * @throws ApiError
    * do not change it after generation gApi (NOT COMMIT)
    */
   public static contactForm(
     candidateUuid: string,
-    formData: FormData
+    formData: FormData,
+    language?: Languages
   ): CancelablePromise<CandidateAnswerOut> {
     return __request(OpenAPI, {
       method: 'POST',
       url: '/api/client/contact_form',
       query: {
         candidate_uuid: candidateUuid,
+        language: language,
       },
       body: formData,
       mediaType: 'multipart/form-data',
