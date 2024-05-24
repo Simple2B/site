7e0157f80501f04ee498f021a32c8be6a71133dd
nazarkobryn <kobrinnazar05@gmail.com>
2024-05-24 14:54:32 +0200

    delete open api gen

--- front/src/openapi/models/IsAuthenticatedOut.ts
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type IsAuthenticatedOut = {
-    user_uuid: string;
-};
-
--- front/src/openapi/models/VariantQuestion.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type VariantQuestion = {
-    id: number;
-    text: string;
-};
-
