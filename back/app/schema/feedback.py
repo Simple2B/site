af21f2054d9e31c73a7230e8368a023ac9da62b9
nazarkobryn <kobrinnazar05@gmail.com>
2024-06-26 11:05:25 -0400

    fix

--- back/app/schema/feedback.py
+++ back/app/schema/feedback.py
@@ -1,12 +1,12 @@
 from typing import List
-from pydantic import BaseModel, Field, ConfigDict, AnyUrl, TypeAdapter
+from pydantic import BaseModel, Field, ConfigDict, TypeAdapter
 
 
 class FeedBack(BaseModel):
     client_name: str = Field(..., alias="clientName")
     project_name: str | None = Field(..., alias="projectName")
     comment: str
-    link: AnyUrl | None
+    link: str | None
 
     model_config = ConfigDict(
         populate_by_name=True,
