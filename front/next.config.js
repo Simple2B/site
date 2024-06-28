41ab24f881a35ed09085027035cc9451c5181e81
nazarkobryn <kobrinnazar05@gmail.com>
2024-06-28 12:16:40 -0400

    create feedback and refactoring

--- front/next.config.js
+++ front/next.config.js
@@ -3,6 +3,11 @@
 const nextConfig = {
   reactStrictMode: false,
   output: 'standalone',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
