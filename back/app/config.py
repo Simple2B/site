3e573243ceef15025b59476014539e9baeb63a2e
Taraslysak <lysak.ipr@gmail.com>
2025-10-18 16:47:11 +0300

    switched to .com

--- back/app/config.py
+++ back/app/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 test_env_file = os.path.abspath("tests/test.env")
 
-IMG_DOMAIN_SERVER = "https://static.simple2b.net"
+IMG_DOMAIN_SERVER = "https://static.simple2b.com"
 
 
 def get_version() -> str:
--- front/next.config.js
+++ front/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'static.simple2b.net',
+        hostname: 'static.simple2b.com',
         port: '',
       },
     ],
