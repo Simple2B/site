a0d5a1356bc84c59781684ecad7eeeeabc1b4590
Taraslysak <lysak.ipr@gmail.com>
2025-10-19 11:10:44 +0300

    switched to .de domain

--- front/src/app/constants-server.ts
+++ front/src/app/constants-server.ts
@@ -1,4 +1,4 @@
 /**For server components */
-export const IMG_DOMAIN_SERVER = 'https://static.simple2b.com';
+export const IMG_DOMAIN_SERVER = 'https://static.simple2b.de';
 // load package version from package.json
 export const PACKAGE_VERSION = require('../../package.json').version;
